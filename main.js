63c2ae25f318ba71b1a4a50b3a5f63f8348f94e1
Ganesh Bhavanasi <ganesh.bhavanasi@fivium.co.uk>
2025-09-17 09:25:11 +0100

    Solution for coding challenge for L5

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'},
+            ],
+            sizes:[
+                {id: 3301, size:'s'},
+                {id: 3302, size:'m'},
+                {id: 3303, size:'l'},
+                {id: 3304, size:'xl'},
+            ]
         }
     }
 })
